818bcc6b5e2e47291030ca02be158315e10a2220
Sourav6803 <souravbhukta8@gmail.com>
2025-04-17 15:33:36 +0530

    vc

---
+++ src/test.js
@@ -0,0 +1,13 @@
+const arr = [1,2,3,4]
+
+const abc = arr.reduce( acumulator => acumulator = 5)
+
+console.log("abc-->", abc)
+
+let a = 10
+console.log(a++); 
+console.log(a);
+
+console.log(++a);
+
+console.log(typeof NaN)
